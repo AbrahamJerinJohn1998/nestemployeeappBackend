2f00de4cc9900ec385033158c87828f4d97224e9
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-12-11 17:30:47 +0530

    initial

---
+++ src/main/java/com/nest/nestemployeeapp/NestemployeeappApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nestemployeeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestemployeeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestemployeeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/nestemployeeapp/NestemployeeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.nestemployeeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestemployeeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
